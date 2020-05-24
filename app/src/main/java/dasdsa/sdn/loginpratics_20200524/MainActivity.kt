5c0a4b7be677449dd14641f40f72d53fbea8d907
umgonji <umgonji@naver.com>
2020-05-24 11:26:17 +0900

    아이디 비번 로그찍기

--- app/src/main/java/dasdsa/sdn/loginpratics_20200524/MainActivity.kt
+++ app/src/main/java/dasdsa/sdn/loginpratics_20200524/MainActivity.kt
@@ -2,11 +2,23 @@ package dasdsa.sdn.loginpratics_20200524
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loginBtn.setOnClickListener {
+
+            val inputId = idEdt.text.toString()
+            val inputPw = pwEdt.text.toString()
+
+            Log.d("입력한아이디", inputId)
+            Log.d("입력비번", inputPw)
+
+        }
     }
 }
